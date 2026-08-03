d7b8e092b7ae3b6fb4f09d6d5608379826596b13
eli0504167101 <4167101@gmail.com>
2026-08-03 12:12:02 +0300

    Add reservation validation and availability checks

--- backend-repo/src/routes/reservationRoutes.js
+++ backend-repo/src/routes/reservationRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const reservationController = require('../controllers/reservationController');
 
-// מלונות
+
 router.get('/hotels', reservationController.getHotels);
 
-// חיפוש הזמנה לפי שם או אימייל
+
 router.get('/lookup/:query', reservationController.lookupReservation);
 
-// יצירת הזמנה
+
 router.post('/', reservationController.createReservation);
 
-// ביטול הזמנה
+
 router.delete('/:id', reservationController.cancelReservation);
 
 module.exports = router;
